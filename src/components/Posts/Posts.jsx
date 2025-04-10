3cf88aba15cfeaa69b3120d412f763930c4096a6
Muhammed Swalih M <zzray485@gmail.com>
2025-04-10 17:35:16 +0530

    home page view is changed, only quick view, no fresh recomentation. in one row 4 posts

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -33,7 +33,6 @@ function Posts() {
       <div className="moreView">
         <div className="heading">
           <span>Quick Menu</span>
-          <span>View more</span>
         </div>
         <div className="cards">
           {products.map(product => (
@@ -52,34 +51,6 @@ function Posts() {
             </div>
             <div className="date">
             <span>{product.createdAt?.toDate().toDateString() || 'No date'}</span>
-            </div>
-          </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Fresh Recommendations */}
-      <div className="recommendations">
-        <div className="heading">
-          <span>Fresh recommendations</span>
-        </div>
-        <div className="cards">
-          {products.map(product => (
-
-            <div className="card" key={product.id}>
-            <div className="favorite">
-              <Heart />
-            </div>
-            <div className="image">
-              <img src={product.imageUrl} alt={product.name} />
-            </div>
-            <div className="content">
-              <p className="rate">&#x20B9; {product.price}</p>
-              <span className="kilometer">{product.category}</span>
-              <p className="name">{product.name}</p>
-            </div>
-            <div className="date">
-            <span>{new Date(product.createdAt?.seconds * 1000).toDateString()}</span>
             </div>
           </div>
           ))}
